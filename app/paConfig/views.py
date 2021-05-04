daf1d96c8121150017fd80aa4682da51663a000d
Ajay Yadav <ajayadav7@publicisgroupe.net>
2021-05-04 17:43:24 +0530

    updates

--- app/paConfig/views.py
+++ app/paConfig/views.py
@@ -9,24 +9,39 @@ class FetchClientsApiView(APIView):
     permission_classes     = []
     authentication_classes = []
     def get(self, request, format=None):
-        clients = (
-            "ac836a33-f40b-4e6a-942d-0cf35083bdeb",
-            "83363026-a3c6-463c-8ebe-16d4a8a64410",
-            "2c06b165-cabb-4758-893e-829359a6807e",
-            "b1d59edf-d6f3-4c55-8570-e5a29513806f",
-            )
+        clients = [{
+            "label": "client1",
+            "value": "ac836a33-f40b-4e6a-942d-0cf35083bdeb"
+        },{
+            "label": "client2",
+            "value": "83363026-a3c6-463c-8ebe-16d4a8a64410"
+        },{
+            "label": "client3",
+            "value": "2c06b165-cabb-4758-893e-829359a6807e"
+        },{
+            "label": "client4",
+            "value": "b1d59edf-d6f3-4c55-8570-e5a29513806f"
+        }]
+        print(clients)
         return Response(status=200, data=clients)
 
 class FetchStudyGroupApiView(APIView):
     permission_classes     = []
     authentication_classes = []
     def get(self, request, format=None):
-        study_group = (
-            "25f2e2f3-3d2a-4999-a827-be8e5d6c5265",
-            "494f20df-0723-45b9-aad2-3adcb46e04ed",
-            "14bff409-af56-471a-8c03-9a59f74a64c2",
-            "3b586a49-c731-49c6-9bd6-31682bed4898",
-            )
+        study_group = [{
+            "label": "study_group1",
+            "value": "25f2e2f3-3d2a-4999-a827-be8e5d6c5265"
+        },{
+            "label": "study_group2",
+            "value": "494f20df-0723-45b9-aad2-3adcb46e04ed"
+        },{
+            "label": "study_group3",
+            "value": "14bff409-af56-471a-8c03-9a59f74a64c2"
+        },{
+            "label": "study_group4",
+            "value": "3b586a49-c731-49c6-9bd6-31682bed4898"
+        }]
         return Response(status=200, data=study_group)
 
 class ClientBindingsAPIView(APIView):
